a1c7914b6f0bf3a129dc714d91d9fe563576a571
fmudondo <fmudondo@gmail.com>
2023-02-12 21:59:41 +0200

    Netflix Project
    Date: 12/02/23

---
+++ next.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https://image.tmdb.org/t/p/original',
+        hostname: 'image.tmdb.org',
+        port: '3000',
+        pathname: 'fmudondo@gmail.com',
+      },
+    ],
+  },
+}
